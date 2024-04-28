9321ee7e726d5336773bd44556f1a0fbb93562dc
Maksim <przedviralem@gmail.com>
2024-04-28 23:19:43 +0300

    хидер

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import headerLogo from '../../images/header/header-logo.png';
 import './Header.css';
+import { Link } from 'react-router-dom';
+import HomePage from '../../pages/HomePage/HomePage';
 
 const Header = () => {
-  return <div className="header">HEADER</div>;
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="header-logo">
+          <Link to="/">
+            <img src={headerLogo} alt="" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
